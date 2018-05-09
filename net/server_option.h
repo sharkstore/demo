5354982209af2b0be668d759faf79cbdc26857b3
byteink <byteink9@gmail.com>
2018-05-09 23:45:20 +0800

    fix timeouts

--- net/server_option.h
+++ net/server_option.h
@@ -8,8 +8,8 @@ namespace net {
 struct ServerOption {
     size_t io_threads_num = 4;
     size_t max_connections = 50000;
-    size_t read_timeout_ms = 5;
-    size_t write_timeout_ms = 5;
+    size_t read_timeout_ms = 5000;
+    size_t write_timeout_ms = 5000;
     size_t write_queue_capacity = 2000;
 };
 
